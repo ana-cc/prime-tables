9749e069abdb1a138f00fde6f3306756e7eb1c9a
Ana Custura <ana@nestat.org.uk>
2019-02-25 15:57:45 +0100

    Cleans up class attributes according to test

--- grid.py
+++ grid.py
@@ -6,14 +6,15 @@ class PrimeGrid():
     def __init__(self, grid_size):
         # initializes the table grid
         self.grid_size = grid_size
-        self.grid = [[None for x in range(self.grid_size + 1)]
-                     for y in range(self.grid_size + 1)]
+        self.edge = grid_size + 1
+        self.grid = [[None for x in range(self.edge)]
+                     for y in range(self.edge)]
 
         # initializes sieve for this grid after finding the input that is required
-        sieve_input = self.find_limit(self.grid_size)
+        sieve_input = self.find_sieve_input(self.grid_size)
         self.sieve = SieveOfEratosthenes(sieve_input)
 
-    def find_limit(self, total_primes):
+    def find_sieve_input(self, total_primes):
         # finds the upper input needed for the sieve to generate a required total number of primes
         # we approximate number of primes generated for a given input using function x/log(x) - 1
         # we test the input in increments of 1000
@@ -29,8 +30,8 @@ class PrimeGrid():
         results.extend(self.sieve.get_results())
 
         # to build the grid, start the inner loop from diagonal to reduce computations
-        for i in range(0, self.grid_size + 1):
-            for j in range(i, self.grid_size + 1):
+        for i in range(0, self.edge):
+            for j in range(i, self.edge):
                 self.grid[i][j] = self.grid[j][i] = (results[i] * results[j])
 
         # pretty printing for smaller grids, 5 digit padding and centered
