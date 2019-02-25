e2ebf4321e646098ed97466d46e10f471413097c
Ana Custura <ana@nestat.org.uk>
2019-02-25 18:11:34 +0100

    Adds more test cases for sieve

--- test_eratosthenes.py
+++ test_eratosthenes.py
@@ -1,8 +1,13 @@
 from eratosthenes import SieveOfEratosthenes
 from nose.tools import assert_equals
 
+def test_sieve_of_eratosthenes_constructor():
+    sieve = SieveOfEratosthenes(100)
+    assert_equals(100, len(sieve.sieve))
+    assert_equals(False, sieve.sieve[0])
+    assert_equals(False, sieve.sieve[1])
 
-def test_sieve_of_eratosthenes():
+def test_sieve_of_eratosthenes_algorithm():
     # compares results returned by the sieve to the known primes under 100
     sieve = SieveOfEratosthenes(100)
     known_primes_100 = [
@@ -11,10 +16,21 @@ def test_sieve_of_eratosthenes():
     ]
     assert_equals(known_primes_100, sieve.get_results())
 
-
-def test_sieve_of_eratosthenes_large():
+def test_sieve_of_eratosthenes_algorithm_large():
     # tests the sieve with a limit of 2 million
     # compares number of results returned by the sieve to the known number of primes under 1M
     sieve = SieveOfEratosthenes(1000000)
     known_number = 78498
     assert_equals(known_number, len(sieve.get_results()))
+
+def test_sieve_of_eratosthenes_input_negative():
+    try:
+        sieve = SieveOfEratosthenes(-1)
+    except(IndexError):
+        assert True
+
+def test_sieve_of_eratosthenes_other_input():
+    try:
+        sieve = SieveOfEratosthenes('hello')
+    except(TypeError):
+        assert True
