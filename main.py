520374d0ecf5ac1c19a623f5ffc54adfd8dad3a8
Ana Custura <ana@nestat.org.uk>
2019-02-25 18:12:36 +0100

    Separates processing function in main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from grid import PrimeGrid
+import sys
 
-def main():
-    n = input("Enter a grid size:")
+def process(n):
     try:
         if (int(n) <= 0):
             sys.exit("Input must be a positive number greater than 0")
     except ValueError:
         sys.exit("Please input an integer.")
         
+
+def main():
+    n = input("Enter a grid size:")
+    process(n)
     g = PrimeGrid(int(n))
-    g.display_grid()
+    print(g.display_grid())
 
 if __name__ == "__main__":
     main()
