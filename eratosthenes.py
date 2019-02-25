c69857a004a3448993841fba5e1d98f11fcaea6a
Ana Custura <ana@nestat.org.uk>
2019-02-25 13:58:18 +0100

    Simple implementation of the Sieve of Eratosthenes

---
+++ eratosthenes.py
@@ -0,0 +1,21 @@
+class SieveOfEratosthenes():
+    # generates primes up to given limit
+    def __init__(self, limit):
+        self.limit = limit
+        # intitializes all items in sieve to True, i.e marks them all as prime
+        self.sieve = [True for i in range(limit)]
+        # 0 and 1 are not primes
+        self.sieve[0] = False
+        self.sieve[1] = False
+        self.sieve_primes()
+
+    def sieve_primes(self):
+        # for every True item in sieve, mark all its multiples in the sieve as False
+        for i in range(self.limit):
+            if self.sieve[i]:
+                for j in range(i + i, self.limit, i):
+                    self.sieve[j] = False
+
+    def get_results(self):
+        # returns a list of all primes
+        return [x for x in range(self.limit) if self.sieve[x] is True]
