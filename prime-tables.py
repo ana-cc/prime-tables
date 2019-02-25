91e112587c05acd81608d97746333e47be93d136
Ana Custura <ana@nestat.org.uk>
2019-02-25 15:00:23 +0100

    Adds some input sanitization

--- prime-tables.py
+++ prime-tables.py
@@ -1,3 +1,4 @@
+import sys
 import math
 from eratosthenes import SieveOfEratosthenes
 
@@ -41,7 +42,13 @@ class PrimeGrid():
             for row in self.grid
         ]))
 
-
 n = input("Enter a grid size:")
+
+try:
+    if (int(n) <= 0):
+        sys.exit("Input must be a positive number greater than 0")
+except ValueError:
+    sys.exit("Please input an integer.")
+    
 g = PrimeGrid(int(n))
 g.display_grid()
