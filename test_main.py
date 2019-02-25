789c6a6959b6177a8e6127d902c126d122cf0dc7
Ana Custura <ana@nestat.org.uk>
2019-02-25 18:13:10 +0100

    Adds tests for main function

---
+++ test_main.py
@@ -0,0 +1,20 @@
+import main
+
+def test_process_negative_input():
+    try:
+       main.process(-1)
+    except SystemExit:
+        assert(True)
+    
+def test_process_word_input():
+    try:
+        main.process('hello')
+    except SystemExit:
+        assert(True)
+
+def test_process_float_input():
+    try:
+        main.process(4.5)
+    except SystemExit:
+        assert(True)
+
