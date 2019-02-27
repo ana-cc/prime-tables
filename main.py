047f6cf17366907fa75c832ade030db5b943310c
Ana Custura <ana@nestat.org.uk>
2019-02-27 11:11:09 +0100

    Adds better formatting for main function

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from grid import PrimeGrid
 import sys
 
+
 def process(n):
     try:
         if (int(n) <= 0):
             sys.exit("Input must be a positive number greater than 0")
     except ValueError:
         sys.exit("Please input an integer.")
-        
+
 
 def main():
     n = input("Enter a grid size:")
     process(n)
     g = PrimeGrid(int(n))
     print(g.display_grid())
+
 
 if __name__ == "__main__":
     main()
